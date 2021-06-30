2f3ff963e38a751111e2343b356962a6cac28e83
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-06-30 23:56:45 +1000

    Prepare Arrow lib and get static SSL linking working on Debian

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,22 @@
-FROM alpine:3.14 AS builder
+FROM debian:bullseye-slim as builder
 
-RUN apk add g++ clang openssl-dev openssl-libs-static
+RUN apt update && \
+    apt install -y ca-certificates lsb-release wget clang pkg-config libgflags-dev libgoogle-glog-dev && \
+    wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb && \
+    apt install -y ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb && \
+    apt update && \
+    apt install -y libarrow-dev libparquet-dev
 
 WORKDIR /build
 COPY src /build
-RUN clang++ --std=c++20 -Wall -O3 -static -o main main.cc -lssl -lcrypto
 
-FROM alpine:3.14
+# To avoid a segfault with a statically linked pthread, we use "--whole-archive":
+# https://gcc.gnu.org/bugzilla/show_bug.cgi?id=52590
+RUN clang++ --std=c++20 -Wall -O3 -static -o main main.cc -lssl -lcrypto -ldl -Wl,--whole-archive -lpthread -Wl,--no-whole-archive -lgflags -lglog
+
+FROM debian:bullseye-slim
+
+# ca-certificates is required for server certificate verification.
+RUN apt update && apt install -y ca-certificates
 
 COPY --from=builder /build/main .
-
-CMD ["./main"]
--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,26 @@
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
 #include <iostream>
 
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "cpp-httplib/httplib.h"
 
-// TODO(@lgruen): Pass access token for Authorization header.
+DEFINE_string(cloud_storage_access_token, "",
+              "GCS access token, e.g. `gcloud auth print-access-token`");
+DEFINE_string(blob_path,
+              "gcp-public-data--gnomad/release/3.1.1/vcf/genomes/"
+              "gnomad.genomes.v3.1.1.sites.chrY.vcf.bgz.tbi",
+              "the path to the GCS blob to download");
+
 std::string ReadBlob(const std::string& path) {
   httplib::Client gcs_client("https://storage.googleapis.com");
   gcs_client.set_url_encode(true);
   const std::string url = std::string("/") + path + "?alt=media";
-  const auto res = gcs_client.Get(url.c_str());
+  const httplib::Headers headers = {
+      {"Authorization",
+       std::string("Bearer ") + FLAGS_cloud_storage_access_token}};
+  const auto res = gcs_client.Get(url.c_str(), headers);
   if (res) {
     return res->body;
   }
@@ -18,9 +30,9 @@ std::string ReadBlob(const std::string& path) {
 }
 
 int main(int argc, char** argv) {
-  // TODO(@lgruen): Add glob or similar.
-  static constexpr char blob_path[] = "bucket/path";
-  const std::string blob = ReadBlob(blob_path);
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  const std::string blob = ReadBlob(FLAGS_blob_path);
   std::cout << "blob size: " << blob.size() << std::endl;
   return 0;
 }
