9c4b9cb47f4e8c2ea460883fe4e9f3a642e2319f
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-08-11 00:50:52 +1000

    Fix ID tokens for Cloud Run

--- src/client/client.py
+++ src/client/client.py
@@ -6,21 +6,28 @@ import sys
 import scan_service_pb2
 import scan_service_pb2_grpc
 
-import google.auth
+import google.auth.compute_engine
 import google.auth.transport.requests
 import google.auth.transport.grpc
 
+
 CLOUD_RUN_URL = os.getenv('CLOUD_RUN_URL')
 assert CLOUD_RUN_URL
-CLOUD_RUN_DOMAIN = CLOUD_RUN_URL.removeprefix('https://')
+
+def remove_prefix(s: str, prefix: str) -> str:
+    return s[len(prefix):] if s.startswith(prefix) else s
+
+CLOUD_RUN_DOMAIN = remove_prefix(CLOUD_RUN_URL, 'https://')
 
 assert len(sys.argv) > 1
 
-credentials, _ = google.auth.default()
 auth_req = google.auth.transport.requests.Request()
+credentials = google.auth.compute_engine.IDTokenCredentials(
+    auth_req, CLOUD_RUN_URL, use_metadata_identity_endpoint=True
+)
+credentials.refresh(auth_req)
 channel = google.auth.transport.grpc.secure_authorized_channel(credentials, auth_req, f'{CLOUD_RUN_DOMAIN}:443')
 stub = scan_service_pb2_grpc.ScanServiceStub(channel)
 req = scan_service_pb2.LoadRequest()
 req.blob_path.extend(sys.argv[1:])
 resp = stub.Load(req)
-
