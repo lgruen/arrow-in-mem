b11274800b149c3cba4fdb0c40a55c97a142695a
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-08-25 15:27:35 +1000

    wip

--- src/server/main.cc
+++ src/server/main.cc
@@ -5,6 +5,8 @@
 #include <absl/synchronization/blocking_counter.h>
 #include <absl/synchronization/mutex.h>
 #include <absl/time/time.h>
+#include <arrow/compute/exec.h>
+#include <arrow/dataset/dataset.h>
 #include <arrow/io/memory.h>
 #include <arrow/ipc/options.h>
 #include <arrow/ipc/reader.h>
@@ -172,8 +174,9 @@ class GcsReader : public UrlReader {
           kNumThreadPoolWorkers)};
 };
 
-absl::StatusOr<size_t> ProcessUrl(const UrlReader& url_reader,
-                                      const std::string_view url) {
+absl::StatusOr<size_t> ProcessArrowUrl(
+    const UrlReader& url_reader, const std::string_view url,
+    const arrow::compute::Expression& filter_expression) {
   const auto data = url_reader.Read(url);
   if (!data.ok()) {
     return absl::InvalidArgumentError(
@@ -195,28 +198,53 @@ absl::StatusOr<size_t> ProcessUrl(const UrlReader& url_reader,
   }
 
   const auto schema = (*record_batch_file_reader)->schema();
-  while (true) {
-    auto record_batch = (*record_batch_file_reader)->Next();
-    if (!record_batch.ok()) {
-      return absl::InvalidArgumentError(
-          absl::StrCat("Failed to read record batch for ", url, ": ",
-                       record_batch_file_reader.status().ToString()));
-    }
-
-    if (record_batch == nullptr) {
-      break;  // All done.
-    }
-
-    ProcessRecordBatch(*record_batch);
+  const auto record_batch_generator =
+      (*record_batch_file_reader)->GetRecordBatchGenerator();
+  if (!record_batch_generator.ok()) {
+    return absl::InvalidArgumentError(
+        absl::StrCat("Failed to create record batch generator for ", url, ": ",
+                     record_batch_generator.status().ToString()));
   }
 
-  const auto num_rows = (*record_batch_file_reader)->CountRows();
-  if (!num_rows.ok()) {
+  arrow::dataset::InMemoryDataset in_memory_dataset{schema,
+                                                    *record_batch_generator};
+  auto scanner_builder = in_memory_dataset.NewScan();
+  if (!scanner_builder.ok()) {
+    return absl::InvalidArgumentError(
+        absl::StrCat("Failed to create scanner builder for ", url, ": ",
+                     scanner_builder.status().ToString()));
+  }
+
+  (*scanner_builder)->Filter(filter_expression);
+  // We parallelize over URLs already, no need for nested parallelism.
+  (*scanner_builder)->UseThreads(false);
+  const auto scanner = (*scanner_builder)->Finish();
+  if (!scanner.ok()) {
+    return absl::InvalidArgumentError(
+        absl::StrCat("Failed to create scanner for ", url, ": ",
+                     scanner.status().ToString()));
+  }
+
+  size_t num_rows = 0;  // After filtering.
+  const auto scan_status = (*scanner)->Scan(
+      [&num_rows](const arrow::dataset::TaggedRecordBatch tagged_record_batch) {
+        const auto record_batch = tagged_record_batch.record_batch;
+        num_rows += record_batch->num_rows;
+      });
+
+  if (!scan_status.ok()) {
     return absl::InvalidArgumentError(absl::StrCat(
-        "Failed to count rows for ", url, ": ", num_rows.status().ToString()));
+        "Failed to run scanner on ", url, ": ", scan_status.ToString()));
   }
 
-  return *num_rows;
+  return num_rows;
+}
+
+absl::StatusOr<arrow::compute::Expression> BuildFilterExpression(
+    const seqr::QueryRequest& request) {
+  // TODO(@lgruen): implement this!
+  using cp = arrow::compute;
+  return cp::less(cp::field_ref("gnomad_genomes_FAF_AF"), cp::literal(0.5));
 }
 
 class QueryServiceImpl final : public seqr::QueryService::Service {
@@ -227,6 +255,12 @@ class QueryServiceImpl final : public seqr::QueryService::Service {
   grpc::Status Query(grpc::ServerContext* const context,
                      const seqr::QueryRequest* const request,
                      seqr::QueryResponse* const response) override {
+    const auto filter_expression = BuildFilterExpression(*request);
+    if (!filter_expression.ok()) {
+      return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT,
+                          std::string(filter_expression.status().message()));
+    }
+
     const size_t num_arrow_urls = request->arrow_urls_size();
     std::vector<absl::StatusOr<size_t>> results(num_arrow_urls);
     absl::BlockingCounter blocking_counter(num_arrow_urls);
@@ -234,7 +268,7 @@ class QueryServiceImpl final : public seqr::QueryService::Service {
       thread_pool_.Schedule([&url_reader = url_reader_,
                              &url = request->arrow_urls(i),
                              &result = results[i], &blocking_counter] {
-        result = GetNumRows(url_reader, url);
+        result = ProcessArrowUrl(url_reader, url, *filter_expression);
         blocking_counter.DecrementCount();
       });
     }
