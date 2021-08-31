ecb7760af4f7327497df0479ebf6980b01b8f84b
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-08-31 23:17:09 +1000

    Initial string_list_contains_any implementation

--- src/server/string_list_contains_any.cc
+++ src/server/string_list_contains_any.cc
@@ -46,59 +46,31 @@ arrow::Status ExecStringListContainsAny(cp::KernelContext* const ctx,
                                         const cp::ExecBatch& batch,
                                         arrow::Datum* const out) {
   const auto state =
-      static_cast<const StringListContainsAnyState*>(ctx->state());
-  const auto& value_set = state->value_set;
+      static_cast<const StringListContainsAnyState&>(*ctx->state());
+  const auto& value_set = state.value_set;  // Based on SetLookupOptions.
+
+  // The boolean output array has already been preallocated.
+  // See IsIn (scalar_set_lookup.cc).
+  arrow::ArrayData* const output = out->mutable_array();
+  arrow::internal::FirstTimeBitmapWriter writer{
+      output->buffers[1]->mutable_data(), output->offset, output->length};
 
   // To understand the layout of an array of list of strings, see the following
   // sections and particularly the List<List<Int8>> example (where strings would
   // use char instead of Int8).
   // https://arrow.apache.org/docs/format/Columnar.html#variable-size-list-layout
   // https://arrow.apache.org/docs/format/Columnar.html#variable-size-binary-layout
-
-  /*
-    arrow::ArrayData* const output = out->mutable_array();
-    arrow::internal::FirstTimeBitmapWriter writer{
-        output->buffers[1]->mutable_data(), output->offset, output->length};
-
-    const arrow::ListArray list{batch[0].array()};
-    const auto string_array =
-        std::static_pointer_cast<arrow::StringArray>(list.values());
-    const std::string* const string_values = string_array->raw_values();
-
-    const ArrayData& input = *batch[0].array();
-    ArrayData* out_arr = out->mutable_array();
-    if (input.length > 0) {
-      transform(reinterpret_cast<const offset_type*>(input.buffers[1]->data()) +
-                    input.offset,
-                input.buffers[2]->data(), input.length, out_arr->offset,
-                out_arr->buffers[1]->mutable_data());
-    }
-
-    const offset_type* offsets =
-        reinterpret_cast<const offset_type*>(raw_offsets);
-    FirstTimeBitmapWriter bitmap_writer(output, output_offset, length);
-    for (int64_t i = 0; i < length; ++i) {
-      const char* current_data = reinterpret_cast<const char*>(data +
-  offsets[i]); int64_t current_length = offsets[i + 1] - offsets[i]; if
-  (matcher->Match(util::string_view(current_data, current_length))) {
-        bitmap_writer.Set();
-      }
-      bitmap_writer.Next();
-    }
-    bitmap_writer.Finish();
-  },
-          out);
-
-  std::static_pointer_cast<arrow::Int64Array>(array);
-  const auto* const offsets = list.raw_value_offsets();
+  arrow::ListArray lists(batch[0].array());
+  const auto& strings = static_cast<const arrow::StringArray&>(*lists.values());
+  const auto* const list_offsets = lists.raw_value_offsets();
+  // See ListValueLength (scalar_nested.cc).
   arrow::internal::VisitBitBlocksVoid(
-      list.data()->buffers[0], list.offset(), list.length(),
-      [&value_set, &writer, offsets](const int64_t position) {
-        const auto begin = offsets[position];
-        const auto end = offsets[position + 1];
-        for (auto i = offsets[position], end = offsets[position + 1]; i < end;
-             ++i) {
-          if (value_set.contains(string_values[i])) {
+      lists.data()->buffers[0], lists.offset(), lists.length(),
+      [&](const int64_t i) {
+        // See BinaryJoin (scalar_string.cc).
+        const auto end = list_offsets[i + 1];
+        for (auto j = list_offsets[i]; j < end; ++j) {
+          if (!strings.IsNull(j) && value_set.contains(strings.GetView(j))) {
             writer.Set();
             writer.Next();
             return;
@@ -107,34 +79,12 @@ arrow::Status ExecStringListContainsAny(cp::KernelContext* const ctx,
         writer.Clear();
         writer.Next();
       },
-      [&writer]() {
+      [&]() {
         writer.Clear();
         writer.Next();
       });
 
   writer.Finish();
-
-  typename TypeTraits<Type>::ArrayType list(batch[0].array());
-  ArrayData* const out_arr = out->mutable_array();
-  auto out_values = out_arr->GetMutableValues<offset_type>(1);
-  const offset_type* offsets = list.raw_value_offsets();
-  ::arrow::internal::VisitBitBlocksVoid(
-      list.data()->buffers[0], list.offset(), list.length(),
-      [&](int64_t position) {
-        *out_values++ = offsets[position + 1] - offsets[position];
-      },
-      [&]() { *out_values++ = 0; });
-
-  template <typename ValidFunc, typename NullFunc>
-  static void VisitVoid(const ArrayData& arr, ValidFunc&& valid_func,
-                        NullFunc&& null_func) {
-    using c_type = typename T::c_type;
-    const c_type* data = arr.GetValues<c_type>(1);
-    auto visit_valid = [&](int64_t i) { valid_func(data[i]); };
-    VisitBitBlocksVoid(arr.buffers[0], arr.offset, arr.length,
-                       std::move(visit_valid),
-                       std::forward<NullFunc>(null_func));
-  */
 
   return arrow::Status::OK();
 }
